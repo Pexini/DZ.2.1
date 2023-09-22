bac769db71f8f92e107e93e8a5fd1a2451fd3ae0
Pavel Davydov <Davydov.works@gmail.com>
2023-09-22 13:29:33 -0400

    add appveyor

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,2 +1,18 @@
-public class FormTest {
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+    @Test
+    void shouldTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get ("http://localhost:9999/");
+    }
 }
+
