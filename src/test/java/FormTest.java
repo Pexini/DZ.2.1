ca5da9ff800bbaa3fe68dece69e4fe9f3734b99b
Pavel Davydov <Davydov.works@gmail.com>
2023-10-01 00:32:06 -0400

    reformat code

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,16 +1,13 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class FormTest {
+public class FormTest {
     @Test
     void shouldTest() {
         open("http://localhost:9999");
-
         $("[data-test-id=name] input").setValue("Иванов Матвей");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
