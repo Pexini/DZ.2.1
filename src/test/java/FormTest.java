2ef1f69a53ead609857611888f5da590a1a74a24
Pavel Davydov <Davydov.works@gmail.com>
2023-10-01 00:12:35 -0400

    Create code Selenide

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,18 +1,20 @@
-
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class CallbackTest {
+class FormTest {
     @Test
     void shouldTest() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:9999/");
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Иванов Матвей");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
-
